2206e018625c9be86834b0495ad8dfffb05f3ca7
PhaserEditor2D <developers@phasereditor2d.com>
2022-04-27 15:16:51 -0400

    Updates build.

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cd $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs
 ./build.sh
 cp -R _build/html $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs-html/
 
-rm -R $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs-html/html/_source
+rm -R $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs-html/html/_sources
 rm -R $PHASEREDITOR_HOME/PhaserEditor2D-v3-docs-html/html/.doctrees
