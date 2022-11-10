201e43487fb1b00265b796119227e5b2587024c5
PhaserEditor2D <developers@phasereditor2d.com>
2022-11-10 08:51:59 -0500

    update

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3.35.1',
+    VERSION: 'v3.35.2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
